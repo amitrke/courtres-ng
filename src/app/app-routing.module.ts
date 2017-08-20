594746cfd582a566fba3f31d4dbfed41c8ad0c89
amitrke <amitrke@gmail.com>
2017-08-19 22:59:35 -0400

    Fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { AppComponent }   from './app.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/app', pathMatch: 'full' },
+  { path: '', redirectTo: '#app', pathMatch: 'full' },
   { path: 'app',  component: AppComponent }
 ];
 
