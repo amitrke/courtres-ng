952da0a6ccd02cf66c0fcfab776beb82fe15e3f0
amitrke <amitrke@gmail.com>
2017-08-27 22:25:21 -0400

    Google signin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { GoogleSignInSuccess } from 'angular-google-signin';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+
+  private myClientId = '577808984615-h6fgitnl31qu5fsoej8p6sbt51a3vb6j.apps.googleusercontent.com';
+  name = '';
+
+  onGoogleSignInSuccess(event: GoogleSignInSuccess) {
+    let googleUser: gapi.auth2.GoogleUser = event.googleUser;
+    let id: string = googleUser.getId();
+    let profile: gapi.auth2.BasicProfile = googleUser.getBasicProfile();
+    console.log('ID: ' +
+      profile
+        .getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    this.title = profile.getName();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule }    from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
+import { GoogleSignInComponent } from 'angular-google-signin';
 
 import { AppComponent } from './app.component';
 import { HelloDBComponent } from './hello-db/hello-db.component';
-import { HelloDBService } from "./hello-db.service"
+import { HelloDBService } from './hello-db.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    GoogleSignInComponent,
     HelloDBComponent
   ],
   imports: [
