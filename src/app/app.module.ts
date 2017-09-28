14c669d924aad60762198bb4278db765f3b76444
amitrke <amitrke@gmail.com>
2017-09-28 08:16:18 -0400

    Introduced angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { GoogleSignInComponent } from 'angular-google-signin';
 import { AppComponent } from './app.component';
 import { HelloDBComponent } from './hello-db/hello-db.component';
 import { UserService } from './shared/user-service';
+import { LoginComponent } from './login/login.component';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GoogleSignInComponent,
-    HelloDBComponent
+    HelloDBComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdButtonModule, MdCheckboxModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
